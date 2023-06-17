51dbecf409eb1625b65c24f20d84722aebc7d396
msuad <1445617868@qq.com>
2023-06-18 01:08:05 +0800

    first commit

---
+++ MDK-ARM/RTE/_l4-mpu6500/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'l4-mpu6500' 
+ * Target:  'l4-mpu6500' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
